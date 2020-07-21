46cca77bcdd416ccb06f32cacb644285258b5895
Cory Maass <cory.maass@genmills.com>
2020-07-20 21:15:33 -0500

    Initial add of code

---
+++ Components/CodeCamp2020/CodeCamp2020.Data/IPokemonRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using CodeCamp2020.Shared.Models;
+
+namespace CodeCamp2020.Data
+{
+    public interface IPokemonRepository
+    {
+        Task<Pokemon> GetAsync(string id);
+        Task<IEnumerable<Pokemon>> GetAllAsync();
+    }
+}
